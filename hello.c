ddd5ec86dba671e4962ec4d587b4cd111ab42efb
Zhangwuwei <125166zz@gmail.com>
2021-01-13 21:11:18 +0800

    添加项目文件。

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("hello world!");
+	int a, b, c;
+	a = 1;
+	scanf("%d",&b);
+	printf("");
+}
---
+++ hellobBY.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+typedef struct Node
+{
+	int data;
+	struct Node* next;
+}Node;
+Node * Coverlist()
+{//jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+	int a;
+	scanf("%d",&a);
+	printf("hah");
+}
+
